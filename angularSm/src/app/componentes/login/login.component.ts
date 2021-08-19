3643cb4e7d78a4d602f5111853cb3bcd3d375beb
CarlosSelman <carlitosselman@gmail.com>
2021-08-19 11:02:01 -0600

    Nuevas funciones y actualización

--- angularSm/src/app/componentes/login/login.component.ts
+++ angularSm/src/app/componentes/login/login.component.ts
@@ -61,7 +61,6 @@ export class LoginComponent implements OnInit {
             timer: 1500,
           });
         }
-
       },
       (error) => {
         Swal.fire({
--- angularSm/src/app/componentes/navbar/navbar.component.ts
+++ angularSm/src/app/componentes/navbar/navbar.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Usuario } from 'src/app/modelos/usuario.modelo';
 import { UsuarioService } from 'src/app/servicios/usuario.service';
+import Swal from "sweetalert2";
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -8,13 +11,27 @@ import { UsuarioService } from 'src/app/servicios/usuario.service';
 })
 export class NavbarComponent implements OnInit {
   public identidad;
+  public token:any;
 
-  constructor(public _usuarioService: UsuarioService) {
+  constructor(public _usuarioService: UsuarioService,
+    private _router: Router) {
     this.identidad = this._usuarioService.getIdentidad();
    }
 
   ngOnInit(): void {
     console.log(this.identidad);
+  }
+
+  refresh(): void{
+    window.location.reload();
+  }
+
+  cerrarSesion(){
+    this.identidad = null;
+    this.token = null;
+    localStorage.setItem('identidad', JSON.stringify(this.identidad))
+    localStorage.setItem('token', JSON.stringify(this.token));
+    this._router.navigate(['/login']);
   }
 
 }
