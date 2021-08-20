390d3d5ce28de7700e7f59e5dc3c9c5e95e6b4ab
CarlosSelman <carlitosselman@gmail.com>
2021-08-20 16:12:45 -0600

    Cambios y nuevas funciones en Usuarios

--- angularSm/src/app/app.module.ts
+++ angularSm/src/app/app.module.ts
@@ -1,16 +1,34 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-
+// Para los componentes
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// Componentes
 import { LoginComponent } from './componentes/login/login.component';
 import { NavbarComponent } from './componentes/navbar/navbar.component';
 import { InicioComponent } from './componentes/inicio/inicio.component';
 import { ReunionComponent } from './componentes/reunion/reunion.component';
 import { UsuarioComponent } from './componentes/usuario/usuario.component';
+
+// Angular...
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatSortModule} from '@angular/material/sort';
 
 @NgModule({
   declarations: [
@@ -24,9 +42,25 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+
+    MatSliderModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    MatProgressSpinnerModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatTableModule,
+    MatTooltipModule,
+    MatPaginatorModule,
+    MatSortModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angularSm/src/app/componentes/usuario/usuario.component.ts
+++ angularSm/src/app/componentes/usuario/usuario.component.ts
@@ -1,3 +1,50 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Usuario } from 'src/app/modelos/usuario.modelo';
+import { UsuarioService } from "../../servicios/usuario.service";
+declare var $: any;
+
+@Component({
+  selector: 'app-usuario',
+  templateUrl: './usuario.component.html',
+  styleUrls: ['./usuario.component.scss'],
+  providers: [UsuarioService]
+})
+export class UsuarioComponent implements OnInit {
+
+  listUsuarios: Usuario[] = [
+    {_id: "avaedognowjeoigj23423sddfdf",nombres: "",apellidos:"",contrasena:"",usuario: "cselman", correo: "cselman@sopesa.com", tel: "1234 - 5678", rol: "Usuario", estado: "Activo"},
+    {_id: "avaedognocvbcvbcvcbvbcvbcvdfdf",nombres: "",apellidos:"",contrasena:"",usuario: "jperez", correo: "jperez@sopesa.com", tel: "6789 - 1011", rol: "Usuario", estado: "Activo"},
+    {_id: "avaedoswwsfrgggggg3423sddfdf",nombres: "",apellidos:"",contrasena:"",usuario: "abran", correo: "abran@sopesa.com", tel: "1111 - 2222", rol: "Usuario", estado: "Activo"},
+    {_id: "avaedo75wtedrggdg3sddfdf",nombres: "",apellidos:"",contrasena:"",usuario: "ebran", correo: "ebran@sopesa.com", tel: "3333 - 4444", rol: "Usuario", estado: "Activo"},
+    {_id: "avaedog125258ygbnmklfhgfbf23423sddfdf",nombres: "",apellidos:"",contrasena:"",usuario: "adavid", correo: "adavid@sopesa.com", tel: "5555 - 6656", rol: "Usuario", estado: "Activo"},
+    {_id: "76754256dfgdeytghdff6gfb",nombres: "",apellidos:"",contrasena:"",usuario: "ljota", correo: "ljota@sopesa.com", tel: "6377 - 2323", rol: "Usuario", estado: "Activo"}
+   ];
+
+  displayedColumns: string[] = ['usuario', 'correo', 'tel', 'rol', 'acciones'];
+  dataSource = new MatTableDataSource(this.listUsuarios);
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor() {}
+
+   ngOnInit(): void {}
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+}
+
+/*
 import { Component, OnInit } from '@angular/core';
 import { Usuario } from 'src/app/modelos/usuario.modelo';
 import { UsuarioService } from "../../servicios/usuario.service";
@@ -61,4 +108,4 @@ export class UsuarioComponent implements OnInit {
 
 
 }
-
+*/
