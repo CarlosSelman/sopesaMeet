2c0be16d3d5dd0abb6fd9cfeedd5e914c66052d7
CarlosSelman <carlitosselman@gmail.com>
2021-08-24 12:05:39 -0600

    Usuarios con filtro y otras funciones listo

--- angularSm/src/app/componentes/usuario/usuario.component.ts
+++ angularSm/src/app/componentes/usuario/usuario.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
@@ -12,7 +12,7 @@ declare var $: any;
   styleUrls: ['./usuario.component.scss'],
   providers: [UsuarioService]
 })
-export class UsuarioComponent implements OnInit {
+export class UsuarioComponent implements AfterViewInit {
 
 displayedColumns: string[] = ['usuario','correo', 'tel','rol', 'acciones'];
 dataSource = new MatTableDataSource<any[]>();
@@ -29,7 +29,7 @@ dataSource = new MatTableDataSource<any[]>();
    }
 
    ngOnInit(): void {
-     this.obtenerUsuarios();
+     //this.obtenerUsuarios();
    }
 
   obtenerUsuarios(){
@@ -44,15 +44,18 @@ dataSource = new MatTableDataSource<any[]>();
   }
 
   ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
+    //this.dataSource.paginator = this.paginator;
+    //this.dataSource.sort = this.sort;
+    //console.log('\nNgAfterViewInit...\n');
+    //this.obtenerUsuarios();
+      this.dataSource.sort = this.sort;
+      this.dataSource.paginator = this.paginator;
   }
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
-
 
 }
 
